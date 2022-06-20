d083f4d430713d8b9e5f135c49de4301873b252e
Chloe Lim <107890665+chloelimPNGHS@users.noreply.github.com>
2022-06-21 09:25:28 +1200

    skeleton of the base code

---
+++ 00_base_V0.py
@@ -0,0 +1,17 @@
+'''
+base component for the game lucky unicorn
+v0 - skeleton setup
+
+author - Chloe Lim
+CC WSA 2022
+
+'''
+
+# import libraries below this line *********
+
+
+# all functions below this line *********
+
+
+
+# main routine below this line *********
